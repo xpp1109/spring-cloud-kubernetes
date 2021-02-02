d0dcb9a0aeae23b2d4d5b1ffcc0efb76afdaa7d0
时振超 <shizc@mail.open.com.cn>
2021-02-02 10:27:23 +0800

    resttemplate

--- spring-cloud-kubernetes-c2/src/main/java/org/pipi/sck/c2/C2Application.java
+++ spring-cloud-kubernetes-c2/src/main/java/org/pipi/sck/c2/C2Application.java
@@ -4,11 +4,18 @@ package org.pipi.sck.c2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class C2Application {
     public static void main(String[] args) {
         SpringApplication.run(C2Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
